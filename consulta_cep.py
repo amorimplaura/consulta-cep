0f4f42e1f010813ff4fa94734fa2b65afc02fe19
Laura Amorim <amorimplaura@gmail.com>
2026-08-30 17:11:47 -0300

    Trata CEP no formato correto mas inexistente

--- consulta_cep.py
+++ consulta_cep.py
@@ -32,7 +32,13 @@ if __name__ == "__main__":
             if not cep_valido(cep):
                 print("CEP inválido! Digite 8 números, sem espaços ou traços.")
                 continue
+            
             dados = consultar_cep(cep)
+            
+            if dados.get("erro"):
+                print("CEP não encontrado.")
+                continue
+                
             exibir_endereco(dados)
         elif opcao == "2":
             print("Até logo!")
